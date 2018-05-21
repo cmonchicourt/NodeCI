fa61d10891607d750ab53c680f904f2056d9ee88
Cedric <cmonchicourt@micropole.com>
2018-05-21 11:52:25 +0200

    Travis config remodif

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID:
         '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-    mongoURI: 'mongodb://admin:Laurent59m@ds241869.mlab.com:41869/blog_cedric',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
     cookieKey: '123123123',
     redis: 'redis://127.0.0.1:6379'
 };
